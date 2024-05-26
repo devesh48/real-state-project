9fb497f41410c652856dacc244d373f40c88febc
devesh48 <upadhyay.dev48@gmail.com>
2024-05-25 23:50:43 -0400

    Adding the mdole to the db

---
+++ node-service/models/estate.model.js
@@ -0,0 +1,22 @@
+import mongoose, { mongo } from "mongoose";
+
+const estateSchema = new mongoose.Schema({
+    username: {
+        required: true,
+        type: String,
+        unique: true
+    },
+    email: {
+        required: true,
+        type: String,
+        unique: true
+    },
+    password: {
+        required: true,
+        type: String,
+    }
+}, {timestamps: true});
+
+const Estate = mongoose('Estate', estateSchema);
+
+export default Estate;
