9d02972f6ef6ab6daa5293b2c175456eb695d80a
devesh48 <upadhyay.dev48@gmail.com>
2024-05-25 21:27:53 -0400

    Adding pages

--- react-ui/src/App.jsx
+++ react-ui/src/App.jsx
@@ -1,5 +1,22 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom"
+import Home from "./pages/Home"
+import Signin from "./pages/Signin"
+import About from "./pages/About"
+import Profile from "./pages/Profile"
+import Signout from "./pages/Signout"
+
+
 export default function App() {
   return (
-    <div className='text-red-600'>App</div>
+    <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Home></Home>}></Route>
+      <Route path="/sign-in" element={<Signin></Signin>}></Route>
+      <Route path="/sign-out" element={<Signout></Signout>}></Route>
+      <Route path="/about" element={<About></About>}></Route>
+      <Route path="/profile" element={<Home></Home>}></Route>
+      <Route path="/" element={<Profile></Profile>}></Route>
+    </Routes>
+    </BrowserRouter>
   )
 }
